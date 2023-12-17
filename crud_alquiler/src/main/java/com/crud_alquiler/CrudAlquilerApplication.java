ff9c5d39265e8b9d264628b38d98410a7d1bf066
carolina <caroibarra2110@gmail.com>
2023-12-17 16:06:42 -0500

    Creacion proyecto crud_alquiler

---
+++ crud_alquiler/src/main/java/com/crud_alquiler/CrudAlquilerApplication.java
@@ -0,0 +1,13 @@
+package com.crud_alquiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAlquilerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudAlquilerApplication.class, args);
+    }
+
+}
